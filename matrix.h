702124823bc0285ce7a5a2a86159b9b1360b02cf
HigerSkill <higerskill@gmail.com>
2017-08-15 02:34:22 +0700

    Add operation

--- matrix.h
+++ matrix.h
@@ -5,12 +5,27 @@
 #include <stdint.h>
 #include "vector.h"
 
+#include <sstream>
+#include <vector>
+#include <stdint.h>
+#include "vector.h"
+#include <assert.h>
+
+template <typename T>
+class Matrix;
+
+template <typename T, typename F>
+Matrix<T> Apply(const Matrix<T>& a, const F& f);
+
+template <typename T, typename F>
+Matrix<T> ZipApply(const Matrix<T>& a, const Matrix<T>& b, const F& f);
+
 template <typename T>
 class Matrix {
 public:
     Matrix() = default;
     Matrix(const std::vector<std::vector<T>>& matrix)
-	    : M(matrix.size())
+            : M(matrix.size())
     {
         for (uint32_t i = 0; i < matrix[0].size(); i++)
             M[i] = MathArray<T>{matrix[i]};
@@ -21,19 +36,19 @@ public:
             M.push_back(item);
     }
     Matrix(const std::vector<MathArray<T>>& matrix)
-        : M{matrix}
+    : M{matrix}
     {}
     Matrix(const MathArray<T>& vector)
-        : M(vector.Size())
+            : M(vector.Size())
     {
-        for (uint32_t i = 0; i < vector.Size(); ++i) {
+        for (uint32_t i = 0; i < vector.Size(); ++i)
             M[i] = vector[i];
-        }
+
     }
     Matrix(uint32_t rows, uint32_t cols)
-        : M(rows, MathArray<T>(cols))
+            : M(rows, MathArray<T>(cols))
     {}
-	
+
     uint32_t Height() const {
         return static_cast<uint32_t>(M.size());
     }
@@ -41,39 +56,123 @@ public:
     uint32_t Width() const {
         if (!M.size())
             return 0;
-        return static_cast<uint32_t>(M[0].size());
+        return static_cast<uint32_t>(M[0].Size());
     }
 
     Matrix<T>& Transpose();
+    Matrix<T>& InplaceTranspose();
 
-    // element-wise operations
-    Matrix<T> operator+(const Matrix<T>& other) const;
-    Matrix<T>& operator+=(const Matrix<T>& other);
+    /*element-wise operations*/
+    Matrix<T> operator+(const Matrix<T>& other) const {
+        return ::ZipApply(*this, other, [](T& a, const T& b) {
+           a += b;
+        });
+    }
+    Matrix<T>& operator+=(const Matrix<T>& other) {
+        return ZipApply(other, [](T& a, const T& b) {
+           a += b;
+        });
+    }
 
-    Matrix<T> operator-(const Matrix<T>& other) const;
-    Matrix<T>& operator-=(const Matrix<T>& other);
-    Matrix<T>& operator-();
+    Matrix<T> operator-(const Matrix<T>& other) const {
+        return ::ZipApply(*this, other, [](T& a, const T& b) {
+            a -= b;
+        });
+    }
+    Matrix<T>& operator-=(const Matrix<T>& other) {
+        return ZipApply(other, [](T& a, const T& b) {
+            a -= b;
+        });
+    }
+    Matrix<T> operator-() const {
+        return ::Apply(*this, [](MathArray<T>& a) {
+            a = -a;
+        });
+    };
 
-    Matrix<T> operator*(const Matrix<T>& other) const;
-    Matrix<T>& operator*=(const Matrix<T>& other);
+    Matrix<T> operator*(const Matrix<T>& other) const {
+        return ::ZipApply(*this, other, [](T& a, const T& b) {
+            a *= b;
+        });
+    }
+    Matrix<T>& operator*=(const Matrix<T>& other) {
+        return ZipApply(other, [](T& a, const T& b) {
+            a *= b;
+        });
+    }
 
-    Matrix<T> operator/(const Matrix<T>& other) const;
-    Matrix<T>& operator/=(const Matrix<T>& other);
+    Matrix<T> operator/(const Matrix<T>& other) const {
+        return ::ZipApply(*this, other, [](T& a, const T& b) {
+            a /= b;
+        });
+    }
+    Matrix<T>& operator/=(const Matrix<T>& other) {
+        return ZipApply(other, [](T& a, const T& b) {
+            a /= b;
+        });
+    }
 
-    Matrix<T> operator*(const T& value) const;
-    Matrix<T>& operator*=(const T& value);
+    Matrix<T> operator*(const T& value) const {
+        return ::Apply(*this, [&](MathArray<T>& a) {
+           a *= value;
+        });
+    }
+    Matrix<T>& operator*=(const T& value) {
+        return Apply([&](MathArray<T>& a) {
+            a *= value;
+        });
+    };
 
-    Matrix<T> operator/(const T& value) const;
-    Matrix<T>& operator/=(const T& value);
+    Matrix<T> operator/(const T& value) const {
+        return ::Apply(*this, [&](MathArray<T>& a) {
+            a /= value;
+        });
+    }
+    Matrix<T>& operator/=(const T& value) {
+        return Apply([&](MathArray<T>& a) {
+            a /= value;
+        });
+    }
 
     // matrix operations
-    Matrix<T>& Dot(const Matrix<T>& other);
+    Matrix<T>& Dot(const Matrix<T>& other) {
+        assert(Width() == other.Height());
+        std::vector<MathArray<T>> result(Height(),  MathArray<T>(other.Width()));
+        for (uint32_t i = 0; i < Height(); ++i) {
+            for (uint32_t j = 0; j < other.Width(); ++j) {
+                for (uint32_t k = 0; k < Width(); ++k) {
+                    result[i][j] += M[i][k] * other.M[k][j];
+                }
+            }
+        }
+        M = result;
+        return *this;
+    }
 
     template <typename F>
-    void Apply(const F& function);
+    Matrix<T>& Apply(const F& f);
 
-    MathArray<T>& operator[](uint32_t i);
-    const MathArray<T> operator[](uint32_t i) const;
+    template <typename F>
+    Matrix<T>& ZipApply(const Matrix<T>& other, const F& f);
+
+    MathArray<T>& operator[](uint32_t i) {
+        if (i >= Height()) {
+            std::stringstream errorDescription;
+            errorDescription << "Out of range: trying to access element "
+                             << i << " while matrix height is " << Height();
+            throw std::out_of_range{errorDescription.str()};
+        }
+        return M[i];
+    }
+    const MathArray<T> operator[](uint32_t i) const {
+        if (i >= Height()){
+            std::stringstream errorDescription;
+            errorDescription << "Out of range: trying to access element "
+                             << i << " while matrix height is " << Height();
+            throw std::out_of_range{errorDescription.str()};
+        }
+        return M[i];
+    };
 
     Matrix<T> VStack(const Matrix<T>& other) const;
 
@@ -84,6 +183,46 @@ private:
     std::vector<MathArray<T>> M;
 };
 
-operator*(const T& value, const Matrix<T>& matrix);
+template <typename T>
+template <typename F>
+Matrix<T>& Matrix<T>::Apply(const F& f) {
+    std::for_each(M.begin(), M.end(), f);
+    return *this;
+}
+
+template <typename T, typename F>
+Matrix<T> Apply(const Matrix<T>& a, const F& f) {
+    Matrix<T> result = a;
+    result.Apply(f);
+    return result;
+}
+
+template <typename T>
+template <typename F>
+Matrix<T>& Matrix<T>::ZipApply(const Matrix<T>& other, const F &f) {
+    if ((Height() != other.Height()) or (M[0].Size() != other.Width())) {
+        std::stringstream errorDescription;
+        errorDescription << "Two matrices must be same size, but "
+                         << "Height first matrix: " << Height()
+                         << " and Height second matrix: " << other.Height()
+                         << "\n Width first matrix: " << Width()
+                         << " and Width second matrix: " << other.Width()
+                         << " were given.";
+        throw std::length_error{errorDescription.str()};
+    }
+    for (uint32_t i = 0; i < Height(); ++i) {
+        for (uint32_t j = 0; j < Width(); ++j) {
+            f(M[i][j], other.M[i][j]);
+        }
+    }
+}
+
+template <typename T, typename F>
+Matrix<T> ZipApply(const Matrix<T>& a, const Matrix<T>& b, const F& f) {
+    Matrix<T> result = a;
+    result.ZipApply(b, f);
+    return result;
+}
+
 
 #endif //ML_MATRIX_H
